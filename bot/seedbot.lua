61b1d0e683644d99f1a7020b6f1e264997c2870a
ehsan76 <ehsankhosh76@gmail.com>
2016-03-17 13:09:29 -0700

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {155942468,133092769},--Sudo users
+    sudo_users = {155942468,104946995},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
